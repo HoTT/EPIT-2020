139fef5adf93e6bd4f39624942697f2fdebbd52b
Andrej Bauer <Andrej.Bauer@andrej.com>
2021-04-14 16:29:00 +0200

    Remove obsolete comment at the top.

--- 01-introduction-to-hott/exercises.v
+++ 01-introduction-to-hott/exercises.v
@@ -1,5 +1,3 @@
-(* If loading this file in emacs, remove first the last few lines that force to call `hoqtop` and rather use the `_CoqProject` method described in the installation instructions *)
-
 From HoTT Require Import HoTT.
 
 (* This file formalizes the exercises from the lectures "Introduction to HoTT". *)
